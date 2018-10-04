c04dbb4641fdd46c5adfb60d4ef5770e595e6c89
Devon Hayworth <dhayworth95@gmail.com>
2018-10-03 23:45:07 -0500

    Modified original code to work with angular although it broke some styling and I don't know why

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {HomepageComponent} from "./homepage/homepage.component";
+import {BrickPageComponent} from "./brick-page/brick-page.component";
+import {ClaimBrickFormPageComponent} from "./claim-brick-form-page/claim-brick-form-page.component";
+import {BrickSearchPageComponent} from "./brick-search-page/brick-search-page.component";
+
+const routes: Routes = [
+    {path: '', component: HomepageComponent},
+    {path: 'brick', component: BrickPageComponent},
+    {path: 'brick/claim', component: ClaimBrickFormPageComponent},
+    {path: 'search', component: BrickSearchPageComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
