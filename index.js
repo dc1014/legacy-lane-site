aba1e9affa3cf3834c66fc0e3d019b7f643bc7bf
Daniel Casper <dc@danielcasper.com>
2018-10-06 12:51:41 -0500

    Serve content through public folder and inert plugin.

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ const server = Hapi.server({
 
 const init = async () => {
 
+    await server.register(require('inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return h.file('./public/index.html');
+        }
+    });
+
+    process.on('unhandledRejection', (err) => {
+
+        console.log(err);
+        process.exit(1);
+    });
+
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
-
 init();
+
