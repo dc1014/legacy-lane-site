c9ee4862d91c66f62984932e7c92c22b0090874e
Daniel Casper <dc@danielcasper.com>
2018-10-11 08:27:32 -0500

    Add Hapi-Mongodb plugin.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # legacy-lane-site
 Legacy Lane Server and Client
+
+## Supporting Docs
+
+Hapi Coding Convetions - https://hapijs.com/styleguide#coding-conventions
--- plugins/index.js
+++ plugins/index.js
@@ -1,10 +1,21 @@
+const { mongoUser, mongoPass, mongoUrl } = require('./../creds.json');
+
+const dbOpts = {
+    url: `mongodb://${mongoUser}:${mongoPass}@${mongoUrl}`,
+    settings: {
+        poolSize: 10
+    },
+    decorate: true
+};
+
 module.exports = {
     manifest: {
         server: { port: process.env.PORT || 3000 },
         register: {
             plugins: [
                 'inert',
-                { plugin: './example' }
+                { plugin: './example' },
+                { plugin: require('hapi-mongodb'), options: dbOpts }
             ],
             options: {
                 once: true
