4acdd954f2c2561f2f894db9f410032d2924aa44
Daniel Casper <dc@danielcasper.com>
2018-10-15 13:01:13 -0500

    Add long, lat, and optIn fields.

--- models/brick.js
+++ models/brick.js
@@ -9,9 +9,12 @@ const schema = Joi.object().keys({
     importId: Joi.string(),
     installedIn: Joi.number().positive().integer(),
     lastName: Joi.string(),
+    lat: Joi.string(),
     line1: Joi.string(),
     line2: Joi.string(),
-    line3: Joi.string()
+    line3: Joi.string(),
+    long: Joi.string(),
+    optIn: Joi.boolean()
 });
 
 module.exports = { collection, schema };
