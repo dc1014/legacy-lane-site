37ab8c3e9077ec1b7ea921d715f5ca1db5a6bb95
Daniel Casper <dc@danielcasper.com>
2018-10-24 16:06:09 -0500

    Finish brick route.

--- models/brick.js
+++ models/brick.js
@@ -7,15 +7,15 @@ const { schema: request } = require('./request.js');
 const schema = Joi.object().keys({
     classOf: Joi.number().positive().integer(),
     comment: Joi.string().required(),
-    email: Joi.string().email(),
+    email: Joi.string().email().lowercase(),
     firstName: Joi.string(),
     giftDate: Joi.date(),
     installedIn: Joi.number().positive().integer(),
     lastName: Joi.string(),
     lat: Joi.string(),
     line1: Joi.string().required(),
-    line2: Joi.string().required(),
-    line3: Joi.string().required(),
+    line2: Joi.string().required().default(''),
+    line3: Joi.string().required().default(''),
     long: Joi.string(),
     image: Joi.string().uri({
         scheme: [
@@ -23,7 +23,7 @@ const schema = Joi.object().keys({
         ]
     }),
     optIn: Joi.boolean(),
-    request,
+    request: request.optional(),
     tags: Joi.array().items(Joi.string()).default([], 'empty tags array')
 });
 
