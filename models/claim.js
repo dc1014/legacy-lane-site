b8e54b07a033e66663052c55732f7849a974af44
Daniel Casper <dc@danielcasper.com>
2018-10-27 00:12:33 -0500

    Add required fields for claim model.

--- models/claim.js
+++ models/claim.js
@@ -2,10 +2,10 @@ const Joi = require('joi');
 
 const schema = Joi.object().keys({
     comment: Joi.string(),
-    email: Joi.string().email().lowercase(), // searchable, private
-    firstName: Joi.string(), // searchable, private
-    lastName: Joi.string(), // searchable, private
-    phoneNumber: Joi.string(), // searchable, private
+    email: Joi.string().email().lowercase().required(), // searchable, private
+    firstName: Joi.string().required(), // searchable, private
+    lastName: Joi.string().required(), // searchable, private
+    phoneNumber: Joi.string().required(), // searchable, private
     tags: Joi.array().items(Joi.string()),
     activity: Joi.date().timestamp().default(Date.now, 'request creation date'),
     optIn: Joi.boolean().default(false),
